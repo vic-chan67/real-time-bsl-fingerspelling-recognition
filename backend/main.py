61d01657a49b7682ebfec2b47b0bf33022d0ae03
Victoria Chan <victoriachan67@gmail.com>
2026-02-22 13:05:40 +0000

    Capture video input

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2 as cv
+
+capture = cv.VideoCapture(0)
+if not capture.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    # ret: frame successfully read (True/False)
+    ret, frame = capture.read()
+
+    # If frame read correctly, ret = True
+    if not ret:
+        print("Can't receive frame. Exiting...")
+        break
+
+    # Operations
+
+    # Display resulting frame
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+
+# When everything done, release the capture
+capture.release()
+cv.destroyAllWindows()
